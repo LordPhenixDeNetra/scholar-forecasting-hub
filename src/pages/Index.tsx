43fbc102c85f465a12dc387e1e0b54c2b70d26cf
LordPhenixDeNetra <netrathior@gmail.com>
2025-02-17 21:02:02 +0000

    Update API-URL

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -10,7 +10,7 @@ const Index = () => {
 
   const handleSubmit = async (data: StudentInput) => {
     try {
-      const response = await fetch("http://localhost:8000/predict", {
+      const response = await fetch("http://ec2-3-95-15-204.compute-1.amazonaws.com/:8000/predict", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
